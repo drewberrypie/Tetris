38e5d380199ceebad6695aced41f1fa33cafe525
drewberrypie <drewazevedo@gmail.com>
2016-03-11 17:40:22 -0500

    ShapeProxyTest

--- TetrisProjectTest/ShapeProxyTest.cs
+++ TetrisProjectTest/ShapeProxyTest.cs
@@ -15,12 +15,30 @@ namespace TetrisProjectTest
             Shape s = new ShapeT(b);
         }
         [TestMethod]
+        public void DeployNewShape_NotNull()
+        {
+            Board b = new Board();
+            ShapeProxy s = new ShapeProxy(b);
+            s.DeployNewShape();
+            Assert.IsNotNull(s);
+        }
+        [TestMethod]
         public void OnJoinPile_EventIsRaised()
         {
             //Assemble
             Board b = new Board();
-            IShape s = b.Shape;
+            Console.WriteLine(b.ToString());
+            IShape s = null;
+            try {
+                Console.WriteLine("Try block: "+b.Shape);
+                s = b.Shape;
+            } catch (NullReferenceException e)
+            {
+                Console.WriteLine("b.Shape exception");
+            }
+            Console.WriteLine(s.ToString());
             List<string> receivedEvents = new List<string>();
+            
             s.JoinPile += delegate(IShape shape)
             {
                 receivedEvents.Add("test true");
@@ -28,8 +46,8 @@ namespace TetrisProjectTest
             //Act
             s.Drop();
             //Assert
-
-
+            Assert.AreEqual(1, receivedEvents.Count);
+            Assert.AreEqual("test true", receivedEvents[0]);
         }
     }
 }
