95678fc6051eb528d54b250c135b37ac1083e68f
drewberrypie <drewazevedo@gmail.com>
2016-03-31 11:43:16 -0400

    Tetris Game after error

--- TetrisGame/Properties/AssemblyInfo.cs
+++ TetrisGame/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b5439e92-b1cc-4add-a94b-1bb573b94956")]
+[assembly: Guid("9f4e8cba-5384-43e4-a155-fe9603447b9a")]
 
 // Version information for an assembly consists of the following four values:
 //
